b4fb4e9eec9deb38f5381009ef887dc80c452a81
unknown <simon@SPBVO-WS04.ostyserver.net>
2013-05-15 12:41:36 +0400

    Use javascript for css substitution.

---
+++ fonts.js
@@ -0,0 +1,21 @@
+var css =
+  ".ii, .Ak { /* Message view */" +
+  "  font: 10pt Consolas !important;" +
+  "}" +
+  "" +
+  ".im, .gmail_quote { /* Quoted Text */" +
+  "  color: #555 !important;" +
+  "}" +
+  "" +
+  ".LW-avf { /* Compose window */" +
+  "  font: normal small Consolas !important;" +
+  "}";
+
+var heads = document.getElementsByTagName("head");
+if (heads.length <= 0)
+  return;
+
+var node = document.createElement("style");
+node.type = "text/css";
+node.appendChild(document.createTextNode(css));
+heads[0].appendChild(node);
